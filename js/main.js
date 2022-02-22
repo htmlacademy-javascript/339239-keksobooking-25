ffb677e4b9ccfb4374ce59f394f0c1e7d640aad0
Konung <mreader212@gmail.com>
2022-02-22 12:17:05 +0300

    Добавил функции для получения случайного целого числа и случайного числа с плавающей точкой с заданной точностью

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const getRandomInteger = (smallestValue, greatestValue) => {
+  // Проверки валидности аргументов
+  if (smallestValue >= 0 && greatestValue > 0) {
+    if (smallestValue <= greatestValue) {
+      return Math.round(smallestValue + Math.random() * (greatestValue - smallestValue));
+    }
+    console.log('Ошибка! Первое значение диапазона должно быть меньше последнего');
+  } else {
+    console.log('Ошибка! В диапазоне не может быть отрицательных чисел.');
+  }
+};
+
+const getRandomFloatingPoint = (smallestValue, greatestValue, resultPrecision) => {
+  // Конвертируем переданную точность в формат для вычислений
+  resultPrecision = 10**resultPrecision;
+
+  // Проверки валидности аргументов
+  if (smallestValue >= 0 && greatestValue > 0) {
+    if (smallestValue <= greatestValue) {
+      return Math.round((smallestValue + Math.random() * (greatestValue - smallestValue)) * resultPrecision) / resultPrecision;
+    }
+    console.log('Ошибка! Первое значение диапазона должно быть меньше последнего');
+  } else {
+    console.log('Ошибка! В диапазоне не может быть отрицательных чисел.');
+  }
+};
