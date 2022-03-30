d870fdb782c76939e7b074cb8810e882afe20be5
Konung <mreader212@gmail.com>
2022-03-30 20:59:29 +0300

    Добавил функции перевода страницы в активное/неактивное состояние, пока вызвал их в main.js чтобы линтер не ругался

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import {createOfferCard} from './render.js';
 import {createAdvertisement} from './data.js';
+import {setStateActive, setStateDisabled} from './states.js';
 import './validation.js';
 
 const advertisements = Array.from({length: 1}, createAdvertisement);
 
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(createOfferCard(advertisements[0]));
+
+setStateActive();
+setStateDisabled();
